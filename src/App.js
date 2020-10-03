e4fde01fca095be73ce41d978db28cbe677d27d4
Aaron Jones <aaron.jones@kibocommerce.com>
2020-10-03 17:41:02 -0500

    react, dev-server, webpack, hot, scss

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { hot } from 'react-hot-loader/root'
+import './styles/app.scss';
+
+
+function App() {
+  return (
+    <div className="app-container">
+          Oh Snap.
+    </div>
+  );
+}
+
+export default hot(App);
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+
+module.exports = {
+    devtool: "source-map",
+    devServer: {
+        open: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.module\.s(a|c)ss$/,
+          loader: [
+            isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                sourceMap: isDevelopment
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment
+              }
+            }
+          ]
+        },
+        {
+          test: /\.s(a|c)ss$/,
+          exclude: /\.module.(s(a|c)ss)$/,
+          loader: [
+            isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment
+              }
+            }
+          ]
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.scss']
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+          filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+          chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+        }),
+        new HtmlWebPackPlugin({
+          template: './dist/index.html',
+          filename: './index.html'
+        })
+      ]
+  }
